ec8fd3e53c48f9943a611c34db35edaeaba6b11a
Bhawani Singh <bs190520@gmail.com>
2025-03-20 16:21:35 +0530

    handleChange and Submit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import { useState } from 'react'
 import login_veg from '@/assets/smlogin.png'
 import sabjiwala_wordmark from '@/assets/sabjiwala_wordmark.png'
 
 const Login = () => {
+  const [formData, setformData] = useState({
+    phone: "",
+    otp: "",
+    otpVerified: ""
+  })
+  console.log(formData)
+
+  const handleChange = (e) => {
+    // regex count 10 digit, only include first digit that is non-zero 
+    const regex = /^[1-9]\d{9}$/;
+    const {id, value } = e.target
+    if (id === 'phone' && regex.test(value) ) {
+      setformData((prevformData) => ({...prevformData, phone : value}))
+      console.log(formData)
+    } else {
+      console.log('invalid phone number')
+    }
+  }
+  const handleSubmit = (e) => {
+
+    const number = formData.phone
+    console.log(number.length)
+    if (number.length == 10){
+      console.log(number, 'from if')
+    } else {
+      console.log(number, 'from else')
+      e.preventDefault();
+    }
+  }
+
+
   return (
     <div className='flex justify-center items-center h-screen'>
       <div>
         <img src={login_veg} alt="Laal Tamatar" className='absolute top-0 left-0' />
-        <form action="submit" method="post" className='mx-8 min-w-[19rem] flex flex-col items-center'>
+        <form action='submit' method='post' onSubmit={handleSubmit} autoComplete='on' className='mx-8 min-w-[19rem] flex flex-col items-center'>
           <img src={sabjiwala_wordmark} alt="Welcome to Sabjiwala" className='max-w-40' />
           <h3 className='mt-9 font-inria font-bold text-xl text-center'>Freshest Fruits & Veggies, <br /> Delivered to Your Doorstep!</h3>
           <div className='mt-9 flex justify-center items-center'>
@@ -18,12 +49,11 @@ const Login = () => {
           <div className='min-w-56 min-h-10 mt-5  py-[10.5px] border-[#999999] border-[0.4px] rounded-[10px]'>
             <div className='flex justify-center items-center'>
               <p>+91</p>
-              <input type="number" name="" id="" placeholder='Enter Phone Number' className=' max-w-36 ml-3 placeholder:font-inria placeholder:font-normal placeholder:text-base' />
-
+              <input type="number" name="phone" id="phone" placeholder='Enter Phone Number' onChange={handleChange} maxLength={10} minLength={3} className=' max-w-36 ml-3 placeholder:font-inria placeholder:font-normal placeholder:text-base' />
             </div>
           </div>
           <button type="submit" className='mt-5 min-w-56 min-h-[3.13rem] bg-[#286C11] rounded-3xl text-white font-inria font-bold text-base'>Continue</button>
-        <p className='absolute bottom-10 max-w-52 text-[#999999] text-[0.8125rem] text-center '>By continuing, you agree to our Terms of Service Privacy Policy Content Policy</p>
+          <p className='absolute bottom-10 max-w-52 text-[#999999] text-[0.8125rem] text-center '>By continuing, you agree to our Terms of Service Privacy Policy Content Policy</p>
         </form>
       </div>
     </div>

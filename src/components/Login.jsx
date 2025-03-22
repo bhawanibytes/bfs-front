42f084ce8e219c95e66da8b25977de9da5cf8dee
Bhawani Singh <bs190520@gmail.com>
2025-03-22 18:18:43 +0530

    restruc using useRef

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,22 +6,21 @@ import { useRef } from 'react'
 
 const Login = () => {
   const dispatch = useDispatch()
-  const phoneRef = useRef('')
+  const phoneRef = useRef()
 
-  const handleChange = (e) => {
+  const handleChange = () => {
     // regex count 10 digit, only include first digit that is non-zero 
     const regex = /^[1-9]\d{9}$/;
-    if (regex.test(e.target.value)) {
-      phoneRef.current = e.target.value
-      dispatch(setPhone(e.target.value))
-      console.log(e.target.value)
+    if (regex.test(phoneRef.current.value)) {
+      dispatch(setPhone(phoneRef.current.value))
     } else {
       console.log('invalid phone number')
     }
   }
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    const phone = phoneRef.current
+    const phone = phoneRef.current.value
     const regex = /^[1-9]\d{9}$/;
     if (regex.test(phone) && phone.length == 10) {
       console.log(phone, 'from if')
@@ -45,7 +44,7 @@ const Login = () => {
         <div className='min-w-56 min-h-10 mt-5  py-[10.5px] border-[#999999] border-[0.4px] rounded-[10px]'>
           <div className='flex justify-center items-center'>
             <p>+91</p>
-            <input type="number" name="phone" id="phone" placeholder='Enter Phone Number' onChange={handleChange} maxLength={10} minLength={3} className=' max-w-36 ml-3 placeholder:font-inria placeholder:font-normal placeholder:text-base' />
+            <input type="number" name="phone" id="phone" ref={phoneRef} placeholder='Enter Phone Number' onChange={handleChange} maxLength={10} minLength={3} className=' max-w-36 ml-3 placeholder:font-inria placeholder:font-normal placeholder:text-base' />
           </div>
         </div>
         <button type="submit" className='mt-5 min-w-56 min-h-[3.13rem] bg-[#286C11] rounded-3xl text-white font-inria font-bold text-base'>Continue</button>
